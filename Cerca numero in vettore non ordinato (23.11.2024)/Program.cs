ba896f04ad3cf5737f42ce432a83af8ab59ec141
Thomas Casali <casali.thomas@gmail.com>
2024-12-02 10:53:00 +0100

    esercizio 1 della verifica del 29.11.2024

---
+++ Cerca numero in vettore non ordinato (23.11.2024)/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Istanzia un vettore da 1000 numeri interi e riempilo con numeri casuali da 1 a 1000
+Chiedere quindi all'utente l'inserimento di un numero N compreso tra 1 e 1000 e
+informare l'utente se il numero inserito è presente nel vettore e in quale posizione 
+(nel caso in cui il numero sia presente più volte stamperà più volte la posizione del
+numero, ad esempio, "TROVATO NELLA POSIZIONE 2")
+ */
+
+var vettore = new int[1000]; //vettore di interi
+int n; //numero inserito dall'utente
+var rnd = new Random(); //variabile per estrazione casuale
+
+//riempimento del vettore
+for(int i = 0; i < 1000; i++)
+{
+    vettore[i] = rnd.Next(1, 1001);
+    //Console.Write(vettore[i] + " ");
+}
+
+do
+{
+    Console.WriteLine("Inserisci un numero compreso tra 1 e 1000");
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n < 1 || n > 1000) Console.WriteLine("Il numero inserito non è nell'intervallo richiesto");
+} while (n < 1 || n > 1000);
+
+for (int i = 0; i < 1000; i++)
+{
+    if (vettore[i] == n) Console.WriteLine("Il numero ricercato è presente alla posizione " + i);
+}
+
+
+
+
+
+
---
+++ Verifica_29.11.2024_esercizio_1/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ Sviluppare un algoritmo in codice C# con le seguenti funzionalità:
+riempie un vettore di N (N deve essere un numero pari) di interi positivi casuali
+calcola la somma dei valori di posto pari e dei valori di posto dispari (0 lo consideriamo pari, per cui faremo la somma dei posti 0,2,4,6 etc… e dei posti 1,3,5,...)
+confronta la somma dei termini posto pari con quella dei termini di posto dispari
+stampa la somma dei termini di posto maggiore informando l’utente se è la somma dei posti pari o dei posti dispari
+ */
+
+//lettura dimensione del vettore
+int n;
+do
+{
+    Console.WriteLine("Inserisci un numero pari");
+    n=Convert.ToInt32(Console.ReadLine());
+    if (n % 2 != 0) Console.WriteLine("Il numero inserito non va bene perché è dispari");
+} while (n%2!=0);
+
+//dichiarazione e estrazione
+var vettore = new int[n];
+Random rnd = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    vettore[i] = rnd.Next(1,100);
+}
+
+//sommo le posizioni pari e le posizioni dispari del vettore
+int sommaPari = 0, sommaDispari = 0;
+for (int i = 0; i < n; i++)
+{
+    if (i % 2 != 0)
+    {
+        sommaDispari += vettore[i];
+    }
+    else
+    {
+        sommaPari += vettore[i];
+    }
+}
+
+//confronta e stampa
+if (sommaPari > sommaDispari)
+{
+    Console.WriteLine("La somma dei posti pari è maggiore della somma dei posti dispari");
+}
+else if (sommaPari < sommaDispari)
+{
+    Console.WriteLine("La somma dei posti dispari è maggiore della somma dei posti pari");
+}
+else
+{
+    Console.WriteLine("La somma dei posti dispari è uguale alla somma dei posti pari");
+}
+Console.WriteLine("Somma dei posti pari:" + sommaPari);
+Console.WriteLine("Somma dei posti dispari:" + sommaDispari);
