50e148c598341704dbf0949bd42be3d79847e226
Thomas Casali <casali.thomas@gmail.com>
2024-12-05 09:48:35 +0100

    shift del vettore commentato

--- shift_vettore/Program.cs
+++ shift_vettore/Program.cs
@@ -18,22 +18,23 @@ while (true)
     {
         case ConsoleKey.RightArrow:
             //fare lo shift a destra del vettore
-            aux = vettore[9];
+            aux = vettore[9]; //salvo il valore dell'ultima posizione
             for (int i = 9; i > 0; i--)
             {
                 vettore[i] = vettore[i - 1];
             }
-            vettore[0] = aux;
+            vettore[0] = aux; //metto il valore dell'ultimo posto all'inizio
             Console.WriteLine("");
             for (int i = 0; i < 10; i++) Console.Write(vettore[i] + " ");
             break;
         case ConsoleKey.LeftArrow:
-            aux = vettore[0];
+            //fa lo shift a sinistra del vettore
+            aux = vettore[0]; //salvo il valore della prima posizione
             for (int i = 0; i < 9; i++)
             {
                 vettore[i] = vettore[i+1];
             }
-            vettore[9] = aux;
+            vettore[9] = aux; //metto il valore della prima posizione nell'ultima
             Console.WriteLine("");
             for (int i = 0; i < 10; i++) Console.Write(vettore[i] + " ");
             break;
