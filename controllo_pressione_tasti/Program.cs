9de60096003021b901994dbc44ec170b1629927c
Thomas Casali <casali.thomas@gmail.com>
2024-12-05 09:45:25 +0100

    esercizio shift vettore

---
+++ controllo_pressione_tasti/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Premi un tasto. Premi Esc per uscire.");
+
+while (true)
+{
+    // Legge un tasto dalla console
+    ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true); // `true` evita di mostrare il tasto nella console
+
+    switch (keyInfo.Key)
+    {
+        case ConsoleKey.RightArrow:
+            Console.WriteLine("Hai premuto la freccia a destra!");
+            break;
+        case ConsoleKey.LeftArrow:
+            Console.WriteLine("Hai premuto la freccia a sinistra!");
+            break;
+        case ConsoleKey.UpArrow:
+            Console.WriteLine("Hai premuto la freccia in alto!");
+            break;
+        case ConsoleKey.DownArrow:
+            Console.WriteLine("Hai premuto la freccia in basso!");
+            break;
+        default:
+            Console.WriteLine($"Tasto premuto: {keyInfo.Key}");
+            break;
+    }
+
+    if (keyInfo.Key == ConsoleKey.Escape) // Esce dal ciclo con Esc
+    {
+        Console.WriteLine("Uscita dal programma.");
+        break;
+    }
+}
---
+++ shift_vettore/Program.cs
@@ -0,0 +1,49 @@
+﻿var vettore = new int[10];
+Random rnd = new Random();
+int aux; //variabile di supporto
+
+for (int i = 0; i < 10; i++)
+{
+    vettore[i] = rnd.Next(1, 100);
+    Console.Write(vettore[i] + " ");
+}
+
+
+while (true)
+{
+    // Legge un tasto dalla console
+    ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true); // `true` evita di mostrare il tasto nella console
+
+    switch (keyInfo.Key)
+    {
+        case ConsoleKey.RightArrow:
+            //fare lo shift a destra del vettore
+            aux = vettore[9];
+            for (int i = 9; i > 0; i--)
+            {
+                vettore[i] = vettore[i - 1];
+            }
+            vettore[0] = aux;
+            Console.WriteLine("");
+            for (int i = 0; i < 10; i++) Console.Write(vettore[i] + " ");
+            break;
+        case ConsoleKey.LeftArrow:
+            aux = vettore[0];
+            for (int i = 0; i < 9; i++)
+            {
+                vettore[i] = vettore[i+1];
+            }
+            vettore[9] = aux;
+            Console.WriteLine("");
+            for (int i = 0; i < 10; i++) Console.Write(vettore[i] + " ");
+            break;
+        default:
+            break;
+    }
+
+    if (keyInfo.Key == ConsoleKey.Escape) // Esce dal ciclo con Esc
+    {
+        Console.WriteLine("Uscita dal programma.");
+        break;
+    }
+}
